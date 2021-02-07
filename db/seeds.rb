ed23e5a59cef1ab57e6e79507ce1dcd1e8f46b89
Liubov Tarasenko <liubov.tarasenko@masterofcode.com>
2021-02-08 00:07:42 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+ 10.times do
+    User.create( {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: '1234567890'
+    })
+ end
+ users = User.all.map(&:id)
+ 50.times do
+    Post.create( {
+      title: Faker::Lorem.word,
+      body: Faker::Lorem.paragraphs,
+      user_id: users.sample
+    })
+  end
